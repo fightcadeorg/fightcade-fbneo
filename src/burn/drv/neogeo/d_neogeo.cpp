1ffe3e88f59cd45019aa0dddab5eb90639e1564f
Combone <acombone@gmail.com>
2023-07-02 23:06:25 -0300

    d_neogeo update: new kof98 hack
    added: kof98mpi

--- src/burn/drv/neogeo/d_neogeo.cpp
+++ src/burn/drv/neogeo/d_neogeo.cpp
@@ -20449,6 +20449,45 @@ struct BurnDriver BurnDrvkof98hc = {
 	0x1000,	304, 224, 4, 3
 };
 
+// The King of Fighters '98 Magic Plus Imitate (Hack)
+// Athor Unknown, have bugs changing character in match but playable
+
+static struct BurnRomInfo kof98mpiRomDesc[] = {
+	{ "242mpi-p1.p1", 0x100000, 0x852ca8ed, 1 | BRF_ESS | BRF_PRG }, //  0 68K code
+	{ "242-p2.sp2",   0x400000, 0x980aba4c, 1 | BRF_ESS | BRF_PRG }, //  1
+
+	{ "242-s1.s1",    0x020000, 0x7f7b4805, 2 | BRF_GRA },           //  2 Text layer tiles
+
+	{ "242-c1.c1",    0x800000, 0xe564ecd6, 3 | BRF_GRA },           //  3 Sprite data
+	{ "242-c2.c2",    0x800000, 0xbd959b60, 3 | BRF_GRA },           //  4
+	{ "242-c3.c3",    0x800000, 0x22127b4f, 3 | BRF_GRA },           //  5
+	{ "242-c4.c4",    0x800000, 0x0b4fa044, 3 | BRF_GRA },           //  6
+	{ "242-c5.c5", 	  0x800000, 0x9d10bed3, 3 | BRF_GRA },           //  7
+	{ "242-c6.c6", 	  0x800000, 0xda07b6a2, 3 | BRF_GRA },           //  8
+	{ "242-c7.c7", 	  0x800000, 0xf6d7a38a, 3 | BRF_GRA },           //  9
+	{ "242-c8.c8", 	  0x800000, 0xc823e045, 3 | BRF_GRA },           // 10
+
+	{ "242-m1.m1",    0x040000, 0x4ef7016b, 4 | BRF_ESS | BRF_PRG }, // 11 Z80 code
+
+	{ "242-v1.v1",    0x400000, 0xb9ea8051, 5 | BRF_SND },           // 16 Sound data
+	{ "242-v2.v2",    0x400000, 0xcc11106e, 5 | BRF_SND },           // 17
+	{ "242-v3.v3",    0x400000, 0x044ea4e1, 5 | BRF_SND },           // 18
+	{ "242-v4.v4",    0x400000, 0x7985ea30, 5 | BRF_SND },           // 19
+};
+
+STDROMPICKEXT(kof98mpi, kof98mpi, neogeo)
+STD_ROM_FN(kof98mpi)
+
+struct BurnDriver BurnDrvkof98mpi = {
+	"kof98mpi", "kof98", "neogeo", NULL, "20??",
+	"The King of Fighters '98 Magic Plus Imitate (Hack)\0", NULL, "hack", "Neo Geo MVS",
+	NULL, NULL, NULL, NULL,
+	BDF_GAME_WORKING | BDF_CLONE | BDF_HACK, 2, HARDWARE_PREFIX_CARTRIDGE | HARDWARE_SNK_NEOGEO, GBF_VSFIGHT, FBF_KOF,
+	NULL, kof98mpiRomInfo, kof98mpiRomName, NULL, NULL, NULL, NULL, neogeoInputInfo, neogeoDIPInfo,
+	NeoInit, NeoExit, NeoFrame, NeoRender, NeoScan, &NeoRecalcPalette,
+	0x1000,	304, 224, 4, 3
+};
+
 // The King of Fighters '98 (Combo Plus, GSC2007 & DREAM)
 // GOTVG
 // Prototype - Ivex kof98cb
